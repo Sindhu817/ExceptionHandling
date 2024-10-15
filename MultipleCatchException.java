a69f8f44c1166c6775b0242aaa894d48b989bb90
Sindhu817 <sindhujakreddy@gmail.com>
2024-10-15 12:11:51 +0530

    Update MultipleCatchException.java

--- MultipleCatchException.java
+++ MultipleCatchException.java
@@ -1,19 +1,33 @@
+/*-------A program to take input of number from user and dividing the existing array by it--------*/
 package ExceptionHandling;
-
+import java.util.Scanner;
 public class MultipleCatchException {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		int[] numbers= {1,23,45,65,67};
-		int num1=34;
+		/*--------declaration of array---------*/
+		int[] numbers= {1,23,0,65,67};
+		/*------Creating object for Scanner Class----------*/
+		Scanner sc = new Scanner(System.in);
+		/*----------taking input by user through the keyboard ---------*/
+		System.out.println("Enter a number:");
+		int num1=sc.nextInt();
 		System.out.println("Divison of number");
+		/*------creating try block for throw an exception-------*/
+		try
+		{
 		for(int index=0;index<=numbers.length;index++)
 		{
 			System.out.println("On Dividing"+(numbers[index]+"by"+num1));
 			System.out.println("Quotient"+(numbers[index]/num1));
 			System.out.println("Remainder:"+(numbers[index]%num1));
 		}
-		catch(ArrayIndexOutOfBoundsException|Arithmatic)
+		}
+		/*--------Creating catch block for handle and throw an Exception--------*/
+		catch (ArrayIndexOutOfBoundsException | ArithmeticException exception)
+		{
+			System.err.println("Exception Ouccred."+exception.getMessage());	
+		}
+		System.out.println("==============================================");
 	}
-
 }
+
+
